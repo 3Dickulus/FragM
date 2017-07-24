c1aaba9976302e5a737bdf6852964e88a16e6958
toonfish <3Dickulus@users.noreply.github.com>
2017-07-23 19:14:16 -0700

    Added EXR binaries search paths

--- Fragmentarium-Source/Fragmentarium/GUI/PreferencesDialog.h
+++ Fragmentarium-Source/Fragmentarium/GUI/PreferencesDialog.h
@@ -21,12 +21,15 @@ namespace Fragmentarium {
 	      m_ui.drawGLPathsCheckBox->setChecked(settings.value("drawGLPaths", true).toBool());
 	      m_ui.splineOccCheckBox->setChecked(settings.value("splineOcc", true).toBool());
 	      m_ui.autoRunCheckBox->setChecked(settings.value("autorun", true).toBool());
-              m_ui.saveEasingCheckBox->setChecked(settings.value("saveEasing", true).toBool());
-              m_ui.useDefinesCheckBox->setChecked(settings.value("useDefines", false).toBool());
-              m_ui.refreshRateSpinBox->setValue(settings.value("refreshRate", 20).toInt());
-              m_ui.fpsSpinBox->setValue(settings.value("fps", 25).toInt());
+          m_ui.saveEasingCheckBox->setChecked(settings.value("saveEasing", true).toBool());
+          m_ui.useDefinesCheckBox->setChecked(settings.value("useDefines", false).toBool());
+          m_ui.refreshRateSpinBox->setValue(settings.value("refreshRate", 20).toInt());
+          m_ui.fpsSpinBox->setValue(settings.value("fps", 25).toInt());
 	      m_ui.includePathsLineEdit->setText(settings.value("includePaths", "Examples/Include;").toString());
-              m_ui.stylesheetLineEdit->setText(settings.value("editorStylesheet", "font: 9pt Courier;").toString());
+#ifdef USE_OPEN_EXR
+          m_ui.exrBinPathsLineEdit->setText(settings.value("exrBinPaths", "bin;/usr/bin;").toString());
+#endif // USE_OPEN_EXR
+          m_ui.stylesheetLineEdit->setText(settings.value("editorStylesheet", "font: 9pt Courier;").toString());
 	    };
             ~PreferencesDialog(){
 	    };
@@ -50,14 +53,18 @@ namespace Fragmentarium {
 		settings.setValue("drawGLPaths", m_ui.drawGLPathsCheckBox->isChecked());
 		settings.setValue("splineOcc", m_ui.splineOccCheckBox->isChecked());
 		settings.setValue("autorun", m_ui.autoRunCheckBox->isChecked());
-                settings.setValue("saveEasing", m_ui.saveEasingCheckBox->isChecked());
-                settings.setValue("useDefines", m_ui.useDefinesCheckBox->isChecked());
-                settings.setValue("refreshRate",  m_ui.refreshRateSpinBox->value());
-                settings.setValue("fps", m_ui.fpsSpinBox->value());
-                settings.setValue("includePaths", m_ui.includePathsLineEdit->text());
-                settings.setValue("editorStylesheet", m_ui.stylesheetLineEdit->text());
-        }
-	private:
+        settings.setValue("saveEasing", m_ui.saveEasingCheckBox->isChecked());
+        settings.setValue("useDefines", m_ui.useDefinesCheckBox->isChecked());
+        settings.setValue("refreshRate",  m_ui.refreshRateSpinBox->value());
+        settings.setValue("fps", m_ui.fpsSpinBox->value());
+        settings.setValue("includePaths", m_ui.includePathsLineEdit->text());
+#ifdef USE_OPEN_EXR
+        settings.setValue("exrBinPaths", m_ui.exrBinPathsLineEdit->text());
+#endif // USE_OPEN_EXR
+        settings.setValue("editorStylesheet", m_ui.stylesheetLineEdit->text());
+    }
+
+    private:
             Ui::PreferencesDialog m_ui;
         };
     }
