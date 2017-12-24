c32707903eb75dd8d5b4a8388e650e2d64488a47
toonfish <3Dickulus@users.noreply.github.com>
2017-12-24 14:50:53 -0800

    class EasingItem

--- Fragmentarium-Source/ThirdPartyCode/TimeLine.h
+++ Fragmentarium-Source/ThirdPartyCode/TimeLine.h
@@ -40,7 +40,27 @@ class MainWindow;
 namespace Fragmentarium {
   namespace GUI {
     
-class TimeLineDialog : public QDialog
+//   class EasingItem : public QGraphicsItem
+//   {
+//   public:
+//       EasingItem(EasingInfo ei) : data(ei);
+//       ~EasingItem();
+//       QRectF boundingRect() const
+//       {
+//           qreal penWidth = 1;
+//           return QRectF(-10 - penWidth / 2, -10 - penWidth / 2,
+//                         20 + penWidth, 20 + penWidth);
+//       }
+// 
+//       void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
+//                  QWidget *widget)
+//       {
+//           painter->drawRoundedRect(-10, -10, 20, 20, 5, 5);
+//       }
+//       EasingInfo data;
+//   };
+
+ class TimeLineDialog : public QDialog
     {
       Q_OBJECT
     public:
