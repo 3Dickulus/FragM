fe94a7d010b89d5de4f804d18b4c44291dcf7bbb
toonfish <3Dickulus@users.noreply.github.com>
2018-10-11 17:24:38 -0700

    switched from QOpenGLFunctions_4_1_Compatibility to QOpenGLFunctions_4_1_Core hopefully cures Claude's segfault using Mesa driver

--- Fragmentarium-Source/Fragmentarium/GUI/DisplayWidget.h
+++ Fragmentarium-Source/Fragmentarium/GUI/DisplayWidget.h
@@ -88,7 +88,7 @@ namespace Fragmentarium {
     class CameraControl;
     
     
-    class DisplayWidget : public QOpenGLWidget, protected QOpenGLFunctions_4_1_Compatibility
+    class DisplayWidget : public QOpenGLWidget, protected QOpenGLFunctions_4_1_Core
     {
       Q_OBJECT
     public:
