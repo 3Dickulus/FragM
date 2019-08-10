982a262cde2bf606ac40650777a384b6090a64e3
toonfish <3Dickulus@users.noreply.github.com>
2019-08-10 01:31:31 -0700

    clean up

--- Fragmentarium-Source/Fragmentarium/Parser/Preprocessor.h
+++ Fragmentarium-Source/Fragmentarium/Parser/Preprocessor.h
@@ -14,8 +14,7 @@
 #include "../../SyntopiaCore/Exceptions/Exception.h"
 #include "../../SyntopiaCore/Logging/Logging.h"
 
-#define DBOUT
-// qDebug() << QString(__FILE__).split(QDir::separator()).last() << __LINE__ << __FUNCTION__
+#define DBOUT qDebug() << QString(__FILE__).split(QDir::separator()).last() << __LINE__ << __FUNCTION__
 
 namespace Fragmentarium {
 using namespace GUI;
