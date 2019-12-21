6677a9a6bb7d5224e43be158beb7e93e379c966a
3Dickulus <3Dickulus@users.noreply.github.com>
2019-12-21 01:53:27 -0800

    [bug fix] errorline off by 1 when including Complex.frag

--- Fragmentarium-Source/Fragmentarium/Parser/Preprocessor.cpp
+++ Fragmentarium-Source/Fragmentarium/Parser/Preprocessor.cpp
@@ -156,13 +156,15 @@ void Preprocessor::parseSource(FragmentSource *fs, QString input, QString origin
         if(!in[0].startsWith("#line") && !in[1].startsWith("#line")) {
             if(!hasVertexCode && !hasIncludes && sf != 0 && vers < 200) {
                 in.insert(1, QString("#line %1 %2").arg(1).arg(sf));
+                DBOUT << 1 << sf;
             }
             else {
                 in.insert( 1, QString("#line %1 %2").arg(2).arg(sf) );
+                DBOUT << 2 << sf;
             }
         }
 
-        for (int i = 0; i < in.count(); i++) {
+        for (int i = 1; i < in.count(); i++) {
             if(hasIncludes) {
                 // insert #line directive after #include statement
                 if (in[i].startsWith("#include", Qt::CaseInsensitive) ) {
